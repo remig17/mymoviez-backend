c3247971797c7b333950d126289a2544f381992c
Remi <remigallois@gmail.com>
2023-04-19 14:05:09 +0200

    backend start

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+const apiKey = process.env.TMDB_API_KEY;
+
+router.get('/movies', (req, res) => {
+
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            res.json({movies: data.results})
+        })
+
+})
+
+
+module.exports = router;
+ 
